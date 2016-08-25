52139b7dab81d4d6ce7e2736f4602793fb70dc8b
Saurabh Shubham <Saurabh Shubham>
2016-08-25 11:43:45 +0530

    Tournamnet partial solved.

--- Tournament.cpp
+++ Tournament.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+    long long i,j,n,temp,c=0;
+    cin>>n;
+    long long a[n];
+    for(i=0;i<n;i++)
+        cin>>a[i];
+
+    for(i=0;i<(n-1);i++)
+    {
+        for(j=(i+1);j<n;j++)
+        {
+            temp=a[j]-a[i];
+            if(temp<0)
+                temp=-1*temp;
+            c+=temp;
+        }
+    }
+    cout<<c;
+    return 0;
+}
